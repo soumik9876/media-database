be71c0993263c9f2cc4e04f40f9b61bdc7d4dc1a
soumik9876 <soumik9876@gmail.com>
2023-03-02 22:06:40 +0600

    Added gunicorn to requirements

--- media_database/settings/__init__.py
+++ media_database/settings/__init__.py
@@ -10,13 +10,10 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-SECRET_KEY = env.get_value('SECRET_KEY')
-ENV_TYPE = env.get_value('ENV_TYPE')
-DEBUG = env.get_value('DEBUG')
-
-if ENV_TYPE == "production":
-    from .production import *
-elif ENV_TYPE == "staging":
+if env.str('ENV_TYPE') == 'STAGING':
     from .staging import *
+elif env.str('ENV_TYPE') == 'PRODUCTION':
+    from .production import *
 else:
     from .development import *
+
